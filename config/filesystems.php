64243de90b565b2fd5d6993a7d1c6045d328eb1c
Your Name <muhammedanaspp098@gmail.com>
2026-05-16 22:58:18 +0530

    Configure Supabase Storage

---
+++ config/filesystems.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    'default' => env('FILESYSTEM_DISK', 'local'),
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
+        'supabase' => [
+            'driver' => 's3',
+            'key' => env('SUPABASE_ACCESS_KEY_ID'),
+            'secret' => env('SUPABASE_SECRET_ACCESS_KEY'),
+            'region' => env('SUPABASE_DEFAULT_REGION'),
+            'bucket' => env('SUPABASE_BUCKET'),
+            'endpoint' => env('SUPABASE_ENDPOINT'),
+            'url' => env('SUPABASE_URL'),
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+            'visibility' => 'public',
+        ],
+
+    ],
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
+];
